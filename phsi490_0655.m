59ca49af9f1d8029958d146ed1a50b6626f1b46d
NeonKarearea <cjbpx41@gmail.com>
2024-08-06 17:30:20 +1200

    I am just wanting to change branch

--- phsi490_0655.m
+++ phsi490_0655.m
@@ -13,8 +13,8 @@ pf_for = Flux(1:turning_point);
 pf_back = Flux(turning_point+1:end);
 
 %Ths finds the averages and then the cutoff flux
-[for_flux, for_L] = cutoff_determine_cjbw(McIlwain_forward,pf_for,0,5);
-[back_flux, back_L] = cutoff_determine_cjbw(McIlwain_backward,pf_back,1,5);
+[for_flux, for_L] = cutoff_determine(McIlwain_forward,pf_for,0,5);
+[back_flux, back_L] = cutoff_determine(McIlwain_backward,pf_back,1,5);
 
 fforlabel = strcat("Flux cutoff = ",num2str(for_flux));
 fbacklabel = strcat("Flux cutoff = ",num2str(back_flux));
@@ -23,6 +23,7 @@ lbacklabel = strcat("L shell cutoff = ",num2str(back_L));
 
 figure(1);
 hold on
+grid on
 plot(McIlwain_forward,pf_for,'b-')
 plot(McIlwain_backward,pf_back,'r-')
 plot(0:1:40,for_flux*ones(1,41),'b--')
@@ -34,7 +35,7 @@ text(10,10,fbacklabel,"FontSize",10)
 text(10,10,lforlabel,"FontSize",10)
 text(10,10,lbacklabel,"FontSize",10)
 set(gca,'FontSize',20,'FontWeight','demi')
-title("The polar pass up for the time period 6:55 to 7:20 on the 23^{rd} of January, 2012");
+title("The polar pass up for the time period 6:55 to 7:20 on the 23^{rd} of January, 2012 using the new method");
 xlabel("L-shell (L)");
 ylabel("Proton flux (protons cm^{-2} s^{-1} ster^{-1})");
 legend('P6_o_m_n_i entrance','P6_o_m_n_i exit','Cutoff flux entrance','Cutoff flux exit','Cutoff L-shell entrance', 'Cutoff L-shell exit')
@@ -43,11 +44,12 @@ hold off
 %The P6, P7, and P8 over the pole
 fig = figure(2);
 hold on
+grid on
 plot(datenum(year(1:8:end),1,day_of_year(1:8:end),hour(1:8:end),minute(1:8:end),second(1:8:end)),Omni_directional_P6(1:8:end),'r','LineWidth',1.25)
 plot(datenum(year(1:8:end),1,day_of_year(1:8:end),hour(1:8:end),minute(1:8:end),second(1:8:end)),Omni_directional_P7(1:8:end),'b','LineWidth',1.25)
 plot(datenum(year(1:8:end),1,day_of_year(1:8:end),hour(1:8:end),minute(1:8:end),second(1:8:end)),Omni_directional_P8(1:8:end),'g','LineWidth',1.25)
 scale=axis;
-axis([datenum(2012,1,23,6,55,0) datenum(2012,1,23,7,20,0) 0 scale(4)])
+%axis([datenum(2012,1,23,6,55,0) datenum(2012,1,23,7,20,0) 0 scale(4)])
 datetick('x',15,'keeplimits')
 set(gca,'FontSize',20,'FontWeight','demi')
 title("Proton flux for the P6, P7, and P8 omnidirectional telescopes over a polar pass")
