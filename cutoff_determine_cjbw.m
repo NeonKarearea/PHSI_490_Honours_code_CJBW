68355b0a9e59ba20f1577d11209f59c61d7ab3ae
NeonKarearea <cjbpx41@gmail.com>
2024-11-19 09:59:37 +1300

    Added the changes that were made

--- cutoff_determine_cjbw.m
+++ cutoff_determine_cjbw.m
@@ -6,7 +6,6 @@ function [a,b,c,d,e,f,g,h,j] = cutoff_determine_cjbw(L_shell,flux,MLT,dst,kp,...
         L_shell = L_shell(end:-1:1);
         flux = flux(end:-1:1);
     end
-    cutoff_location=1;
     
     r_earth = 6.371e6; %meters, radius of Earth
     r_sat = 8.50e5; %meters, average orbital height of the POES satellites
@@ -20,6 +19,8 @@ function [a,b,c,d,e,f,g,h,j] = cutoff_determine_cjbw(L_shell,flux,MLT,dst,kp,...
     cutoff_flux = avg_flux/2;
     del_flux = flux-cutoff_flux*ones(size(flux));
 
+    cutoff_location = 1;
+    
     %This is to catch any instances where there is no L-shell above
     %the defined L
     if isnan(avg_flux) == 1
@@ -39,7 +40,7 @@ function [a,b,c,d,e,f,g,h,j] = cutoff_determine_cjbw(L_shell,flux,MLT,dst,kp,...
         sign_del_flux_minus = sign(del_flux(2:end));
         sign_change = sign_del_flux_plus ~= sign_del_flux_minus & (~isnan(sign_del_flux_plus) & ~isnan(sign_del_flux_minus));
         sign_change_loc = find(sign_change == 1);
-        sign_change_loc_groups = find(diff(sign_change_loc) >= 4);
+        sign_change_loc_groups = find(abs(diff(sign_change_loc)) >= 2);
         
         %This is where I gerrymander the data
         for i = 1:length(sign_change_loc_groups)+1
@@ -67,15 +68,26 @@ function [a,b,c,d,e,f,g,h,j] = cutoff_determine_cjbw(L_shell,flux,MLT,dst,kp,...
         if exist("avg_grad_in","var")
             avg_grads_and_fluxes = ([avg_grad_in;avg_grad_out;avg_del_flux_in;avg_del_flux_out])';
             point_validity = double((avg_grads_and_fluxes(:,1)<0 & avg_grads_and_fluxes(:,2)>0)&(avg_grads_and_fluxes(:,3)<0 & avg_grads_and_fluxes(:,4)>0));
+            positives = length(find(point_validity == 1));
             point_difference = diff(point_validity);
             attempted_location = find((point_validity(1:end-1) == 1 & point_difference == 0),1);
-            if isempty(find(point_validity == 1,1))
-                cutoff_location = 1;
+            
+            if positives == 1
+                cutoff_location = int64(sign_change_loc_grouped(point_validity==1));
             elseif isempty(attempted_location)
-                cutoff_location = int64(floor(median(sign_change_loc_grouped(point_validity==1))));
+                cutoff_location = 1;
             else
-                cutoff_location = int64(sign_change_loc_grouped(point_validity(attempted_location)));
+                cutoff_location = 1;%int64(sign_change_loc_grouped(point_validity(attempted_location)));
             end
+            %point_difference = diff(point_validity);
+            %attempted_location = find((point_validity(1:end-1) == 1),1);
+            %if isempty(find(point_validity == 1,1))
+            %    cutoff_location = 1;
+            %elseif isempty(attempted_location)
+            %    cutoff_location = int64(median(sign_change_loc_grouped(point_validity==1)));
+            %else
+            %    cutoff_location = int64(sign_change_loc_grouped(point_validity(attempted_location)));
+            %end
             
             true_flux = flux(cutoff_location);
             true_L = L_shell(cutoff_location);
@@ -92,7 +104,7 @@ function [a,b,c,d,e,f,g,h,j] = cutoff_determine_cjbw(L_shell,flux,MLT,dst,kp,...
     %This is a final catch in case the cutoff flux isn't found and removes
     %passes that at any point touch the SAMA
     front_half_flux = flux(1:floor(length(flux)/2));
-    if ~exist('true_flux','var')||~exist('true_L','var')||true_flux<=min_flux||avg_flux<=min_avg_flux||(length(find(front_half_flux<=min_flux))<num_grad)||cutoff_location==1
+    if ~exist('true_flux','var')||~exist('true_L','var')||true_flux<=min_flux||avg_flux<=min_avg_flux||(length(find(front_half_flux<=min_flux))<num_grad)||cutoff_location == 1
         true_flux = NaN;
         true_L = NaN;
         true_MLT = NaN;
