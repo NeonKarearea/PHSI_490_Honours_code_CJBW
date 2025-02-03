9f4d959607372c6d423669b3e7a0477e8f874035
NeonKarearea <cjbpx41@gmail.com>
2025-02-04 11:12:31 +1300

    Corrected pearson.m as there was a small bug

--- pearson.m
+++ pearson.m
@@ -1,9 +1,9 @@
 function [a] = pearson(x,y)
-    x_resids = x(~isnan(x))-mean(x,'omitnan');
-    y_resids = y(~isnan(y))-mean(y,'omitnan');
-    x_rsm = sqrt(sum(x_resids.^2));
-    y_rsm = sqrt(sum(y_resids.^2));
+    x_resids = x-mean(x,'omitnan');
+    y_resids = y-mean(y,'omitnan');
+    x_rsm = sqrt(sum(x_resids.^2,'omitnan'));
+    y_rsm = sqrt(sum(y_resids.^2,'omitnan'));
     
-    pearson = (sum(x_resids.*y_resids))./(x_rsm.*y_rsm);
+    pearson = (sum(x_resids.*y_resids,'omitnan'))./(x_rsm.*y_rsm);
     a = pearson;
 end
